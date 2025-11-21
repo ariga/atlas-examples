d2fcf208c4da6e9fd756a1059cff54e68ef21da7
Dat Dao <datdao@ariga.io>
2025-11-21 10:52:10 +0700

    cicd: add Bitbucket examples

---
+++ cicd/bitbucket/versioned/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "users" (
+  "id" bigserial PRIMARY KEY,
+  "name" text NOT NULL,
+  "active" boolean NOT NULL,
+  "address" text NOT NULL,
+  "nickname" text NOT NULL,
+  "nickname2" text NOT NULL,
+  "nickname3" text NOT NULL
+);
